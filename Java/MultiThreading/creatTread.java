ae20e056faf171c484af2877d4cddb4c5cc21878
Levon M <100533325+goodluck3301@users.noreply.github.com>
2023-01-18 00:25:18 +0400

    Create creatTread.java

---
+++ Java/MultiThreading/creatTread.java
@@ -0,0 +1,20 @@
+class SomeThread extends Thread {
+    public void run() {
+        for(int i = 0; i< 10; i++)
+            System.out.println("Number - "+i);
+    }    
+}
+
+
+public class Main
+{    
+	public static void main(String[] args) {
+		SomeThread test = new SomeThread();
+		
+		test.start();
+		
+		SomeThread test1 = new SomeThread();
+		
+		test1.start();
+	}
+}
